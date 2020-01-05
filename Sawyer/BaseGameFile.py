f8202357fb1c545aaf0c3f1f2f226bef1ceeccb2
PythonCoderforlife <sawyerp06@gmail.com>
2020-01-04 20:17:58 -0600

    JESUS

--- Sawyer/BaseGameFile.py
+++ Sawyer/BaseGameFile.py
@@ -4,8 +4,19 @@ try:
     Chr1 = Chr(100, 30, True)
     Chr2 = Chr(100, 30, True)
     Chr3 = Chr(100, 30, True)
+except SyntaxError as err:
+    print(err)
+except TypeError as err:
+    print(err)
+except OverflowError as err:
+    print(err)
 except:
+    print("Unkown error")
 
+ChrSelect = input("Chose Character")
+
+def losehealth():
+    print("")
 
 
 
